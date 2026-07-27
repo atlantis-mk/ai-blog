9c70db5888bd7db921b2319ebff4e47dbdfc09dd
atlantis-mk <185823991+atlantis-mk@users.noreply.github.com>
2026-07-27 14:10:43 +0800

    Fix Payload admin import map on Vercel

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -312,7 +312,6 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
-  prefix?: string | null;
   folder?: (number | null) | FolderInterface;
   updatedAt: string;
   createdAt: string;
@@ -1467,7 +1466,6 @@ export interface ReleasesSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
-  prefix?: T;
   folder?: T;
   updatedAt?: T;
   createdAt?: T;
